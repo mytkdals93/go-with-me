c0294749277c1bab86c8edebc40abbe6e592ab04
LEESANGMIN <mytkdals93@gmail.com>
2020-11-19 03:01:53 +0900

    linked list 수정

--- dataStructure/linkedList/linkedList.go
+++ dataStructure/linkedList/linkedList.go
@@ -6,52 +6,111 @@ import (
 	"os"
 )
 
-type linkedList struct {
-	head   *node
-	length int
-}
 type node struct {
 	data int
 	next *node
 }
+type linkedList struct {
+	head   *node
+	tail   *node
+	length int
+}
+
+func (l *linkedList) push(n *node) {
+	if l.head == nil {
+		l.head = n
+		l.tail = n
+		l.length++
+		return
+	}
+	l.tail.next = n
+	l.tail = n
+	l.length++
+
+}
 
 func (l *linkedList) prepend(n *node) {
+	if l.head == nil {
+		l.head = n
+		l.tail = n
+		l.length++
+		return
+	}
 	second := l.head
 	l.head = n
 	l.head.next = second
 	l.length++
 }
-func (l *linkedList) deleteWithValue(value int) (int, error) {
-	if value == l.head.data {
-		l.head = l.head.next
+func (l *linkedList) delete(value int) (int, error) {
+	//Empty List
+	if l.head == nil {
+		err := fmt.Errorf("Empty List")
+		return 0, err
+	}
+
+	//지우고자 하는 값이 HEAD인 경우
+	if l.head.data == value {
+		// HEAD와 TAIL이 같은 경우
+		if l.head == l.tail {
+			l.head = nil
+			l.tail = nil
+		} else {
+			l.head = l.head.next
+		}
 		l.length--
 		return value, nil
 	}
-	prev := l.head
-	for prev.next != nil && prev.next.data != value {
-		prev = prev.next
+
+	temp := l.head
+	for temp.next != nil && temp.next.data != value {
+		temp = temp.next
 	}
-	if prev.next == nil {
+
+	//지우고자 하는 값이 LIST에 없는 경우
+	if temp.next == nil {
 		err := fmt.Errorf("Not Found")
 		return 0, err
 	}
-	if prev.next.next != nil {
-		prev.next = prev.next.next
+
+	//지우고자 하는 값이 TAIL인 경우
+	if temp.next == l.tail {
+		l.tail = temp
+		l.tail.next = nil
 	} else {
-		prev.next = nil
+		temp.next = temp.next.next
 	}
 	l.length--
 	return value, nil
 }
+func (l *linkedList) get(index int) (*node, error) {
+	if index < 0 {
+		err := fmt.Errorf("invalid index: index shouldn't be negative")
+		return nil, err
+	}
+	if !(index < l.length) {
+		err := fmt.Errorf("Out of Length,length:%d index:%d", l.length, index)
+		return nil, err
+	}
+	temp := l.head
+	for index > 0 {
+		temp = temp.next
+		index--
+	}
 
+	return temp, nil
+}
 func (l linkedList) print(w io.Writer) {
 	if w == nil {
 		w = os.Stdout
 	}
 	temp := l.head
 
-	for temp != nil {
-		fmt.Fprint(w, temp.data, "=>")
+	for i := 0; i < l.length; i++ {
+		if i != l.length-1 {
+			fmt.Fprint(w, temp.data, "=>")
+		} else {
+			fmt.Fprint(w, temp.data)
+		}
 		temp = temp.next
 	}
 }
--- dataStructure/linkedList/linkedList_test.go
+++ dataStructure/linkedList/linkedList_test.go
@@ -8,47 +8,129 @@ import (
 )
 
 func makeLinkedList(a ...int) linkedList {
-	mylist := linkedList{}
+	ll := linkedList{}
 	for _, v := range a {
-		mylist.prepend(&node{data: v})
+		ll.push(&node{data: v})
 	}
-	return mylist
+	return ll
+}
+func TestPush(t *testing.T) {
+	assert := assert.New(t)
+	arr := []int{10, 20, 30, 40}
+	ll := linkedList{}
+	for _, v := range arr {
+		ll.push(&node{data: v})
+	}
+	assert.Equal(ll.length, len(arr))
+	assert.Equal(ll.head.data, 10)
+	assert.Equal(ll.tail.data, 40)
+
+	buf := bytes.NewBufferString("")
+	ll.print(buf)
+	assert.Equal("10=>20=>30=>40", buf.String())
 }
 func TestPrepend(t *testing.T) {
 	assert := assert.New(t)
-
-	mylist := makeLinkedList(40, 10, 20, 30)
-	assert.Equal(mylist.length, 4)
-	assert.Equal(mylist.head.data, 30)
-	assert.Equal(mylist.head.next.data, 20)
-	assert.Equal(mylist.head.next.next.data, 10)
-	assert.Equal(mylist.head.next.next.next.data, 40)
+	arr := []int{10, 20, 30, 40}
+	ll := linkedList{}
+	for _, v := range arr {
+		ll.prepend(&node{data: v})
+	}
+	assert.Equal(ll.length, len(arr))
+	assert.Equal(ll.head.data, 40)
+	assert.Equal(ll.tail.data, 10)
+	buf := bytes.NewBufferString("")
+	ll.print(buf)
+	assert.Equal("40=>30=>20=>10", buf.String())
 }
 
-func TestDeleteWithValue(t *testing.T) {
+func TestDelete(t *testing.T) {
 	assert := assert.New(t)
-	mylist := makeLinkedList(40, 10, 20, 30)
-	//-----------------------------------------
-	originLen := mylist.length
-	targetData := 40
-	n, err := mylist.deleteWithValue(targetData)
+	//HEAD 삭제-----------------------------------------
+	ll := makeLinkedList(10, 20, 30, 40, 50)
+	targetData := 10
+	n, err := ll.delete(targetData)
 	assert.Equal(n, targetData, "check the return value")
-
-	assert.Equal(mylist.length, originLen-1, "check length of list")
+	assert.Equal(ll.length, 4, "check length of list")
+	assert.Equal(ll.head.data, 20, "check the value of head")
+	assert.Equal(ll.tail.data, 50, "check the value of tail")
 	buf := bytes.NewBufferString("")
-	mylist.print(buf)
-	assert.Equal("30=>20=>40=>", buf.String(), "check the result of print")
+	ll.print(buf)
+	assert.Equal("20=>30=>40=>50", buf.String(), "check the result of print")
+	assert.NoError(err)
+	//중간값 삭제-----------------------------------------
+	ll = makeLinkedList(10, 20, 30, 40, 50)
+	targetData = 30
+	n, err = ll.delete(targetData)
+	assert.Equal(n, targetData, "check the return value")
+	assert.Equal(ll.length, 4, "check length of list")
+	assert.Equal(ll.head.data, 10, "check the value of head")
+	assert.Equal(ll.tail.data, 50, "check the value of tail")
+	buf = bytes.NewBufferString("")
+	ll.print(buf)
+	assert.Equal("10=>20=>40=>50", buf.String(), "check the result of print")
+	assert.NoError(err)
+	//Tail 삭제-----------------------------------------
+	ll = makeLinkedList(10, 20, 30, 40, 50)
+	targetData = 50
+	n, err = ll.delete(targetData)
+	assert.Equal(n, targetData, "check the return value")
+	assert.Equal(ll.length, 4, "check length of list")
+	assert.Equal(ll.head.data, 10, "check the value of head")
+	assert.Equal(ll.tail.data, 40, "check the value of tail")
+	buf = bytes.NewBufferString("")
+	ll.print(buf)
+	assert.Equal("10=>20=>30=>40", buf.String(), "check the result of print")
 	assert.NoError(err)
 
-	n, err = mylist.deleteWithValue(1000)
-	assert.NoError(err, err.Error())
+	// 데이터가 하나 밖에 없을 경우 -----------------------
+	ll = makeLinkedList(10)
+	targetData = 10
+	n, err = ll.delete(targetData)
+	assert.Equal(n, targetData, "check the return value")
+	assert.Equal(ll.length, 0, "check length of list")
+	assert.Nil(ll.head)
+	assert.Nil(ll.tail)
+	buf = bytes.NewBufferString("")
+	ll.print(buf)
+	assert.Equal("", buf.String(), "check the result of print")
+	assert.NoError(err)
+	// Empty List에서 삭제 시도
+	ll = makeLinkedList()
+	n, err = ll.delete(1)
+	assert.Error(err, err.Error())
+	assert.Contains(err.Error(), "Empty List")
+	//리스트에 존재하지 않는 값 삭제-----------------------------------------
+	ll = makeLinkedList(10, 20, 30, 40, 50)
+	n, err = ll.delete(1000)
+	assert.Error(err, err.Error())
+	assert.Contains(err.Error(), "Not Found")
+}
+
+func TestGet(t *testing.T) {
+	assert := assert.New(t)
+	ll := makeLinkedList(10, 20, 30, 40)
+
+	n, err := ll.get(3)
+	assert.Equal(n, ll.tail)
+	assert.NoError(err)
+	buf := bytes.NewBufferString("")
+	ll.print(buf)
+	assert.Equal("10=>20=>30=>40", buf.String())
+	//index값이 음수 error처리
+	n, err = ll.get(-1)
+	assert.Error(err)
+	//legth보다 index값이 더 큰 경우 error처리
+	n, err = ll.get(ll.length + 1)
+	assert.Error(err)
+
 }
 func TestPrint(t *testing.T) {
 	assert := assert.New(t)
-	mylist := makeLinkedList(40, 10, 20, 30)
+	ll := makeLinkedList(10, 20, 30, 40)
 
 	buf := bytes.NewBufferString("")
-	mylist.print(buf)
-	assert.Equal("30=>20=>10=>40=>", buf.String())
+	ll.print(buf)
+	assert.Equal("10=>20=>30=>40", buf.String())
 
 }
